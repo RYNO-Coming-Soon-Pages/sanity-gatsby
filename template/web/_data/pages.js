b495c32dd0330df464c73a778f39c20b76e33f9b
Chad Mueller <cmueller@rynoss.com>
2021-01-14 16:44:19 -0700

    update sanity client env strategy for dev/prod

--- template/web/_data/pages.js
+++ template/web/_data/pages.js
@@ -1,15 +1,5 @@
 require('dotenv').config();
-const sanityClient = require('@sanity/client');
-
-const projectId = process.env.SANITY_PROJECT;
-//const apiToken = process.env.SANITY_TOKEN;
-const dataset = process.env.SANITY_DATASET;
-
-const client = sanityClient({
-    projectId,
-    dataset,
-    // token: apiToken
-})
+const client = require('../utils/sanityClient.js');
 
 module.exports = async function() {
     const query = `
---
+++ template/web/client-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    sanity: {
+        projectId: process.env.SANITY_PROJECT_ID || '<#< sanity.projectId >#>',
+        dataset: process.env.SANITY_DATASET || '<#< sanity.dataset >#>'
+    }
+}
+  
