03157b4644715a88f3845c5a0c29c87ae9d1600e
Chad Mueller <cmueller@rynoss.com>
2021-01-22 17:08:23 -0700

    Update thumbnail for template preview

--- template/studio/dashboardConfig.js
+++ template/studio/dashboardConfig.js
@@ -32,7 +32,11 @@ export default {
               value: 'https://github.com/<#<repository.owner>#>/<#<repository.name>#>',
               category: 'Code'
             },
-            {title: 'Frontend', value: '<#<deployments.web.url>#>', category: 'apps'}
+            {
+              title: 'Frontend',
+              value: '<#<deployments.web.url>#>',
+              category: 'apps'
+            }
           ]
         }
       },
